6689d89cbcbe067b1ccba2d6a82aa567d9790688
Clément Hélardot <clement.helardot@gmail.com>
2017-12-22 00:13:56 +0100

    Revert "Unit test v1"
    This reverts commit d027fcc582fa89697ceac09a80e020c061087b8d.

--- test/main_test.js
+++
@@ -1,8 +0,0 @@
-var expect = require('chai').expect;
-//Test suite for similar parts of code / functions
-describe('Mocha', function(){
-  //Test spec (unit test)
-  it('Should run our tests using npm', function(){
-    expect(true).to.be.ok;
-  });
-});
